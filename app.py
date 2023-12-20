f22d3c227bcec43eca96d9d0b8912879b6f36903
Brian Gomes <67698602+bdgomey@users.noreply.github.com>
2023-12-20 13:21:55 -0600

    updated code

--- app.py
+++ app.py
@@ -10,6 +10,21 @@ def index():
 def cats():
   return render_template('cats.html')
 
+@app.route('/dogs')
+def dogs():
+  return render_template('dogs.html')
+
+@app.route('/training/cats')
+def cat_training():
+  return render_template('cat_training.html')
+
+@app.route('/training/dogs')
+def dog_training():
+  return render_template('dog_training.html')
+
+@app.route('/why')
+def why():
+  return render_template('dogsvcats.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
